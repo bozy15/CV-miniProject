e5cd5cbacacf9ba9cb1cc2d92126343d431d7499
Emmett Becirovic <76811586+bozy15@users.noreply.github.com>
2021-06-07 11:24:38 +0100

    Fix the bugs for the gh-user-data & gh-repo-data elements

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -36,9 +36,10 @@ function repoInformationHTML(repos) {
           </div>`;
 }
 
-console.table(listItemsHTML)
-
 function fetchGitHubInformation(event) {
+  $("#gh-user-data").html("");
+  $("#gh-repo-data").html("");
+  
   var username = $("#gh-username").val();
   if (!username) {
     $("#gh-user-data").html(`<h2>Please enter a GitHub username</h2>`);
@@ -73,3 +74,5 @@ function fetchGitHubInformation(event) {
     }
   );
 }
+
+$(document).ready(fetchGitHubInformation);
